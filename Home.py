5af6e48cb8052c50896f84acec0eb526fe1c75a8
starlyt / kireina kalila <113534896+stxrlyt@users.noreply.github.com>
2025-05-27 18:29:43 +0700

    homepage + example csv files for file data input

---
+++ Home.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="MCDM",
+    page_icon="📊",
+)
+
+st.title('Multi Criteria Decision Maker')
+
+st.write('Please choose your wanted algorithm on the sidebar.')
